f4d68c4516bb59ee27b15399b0b7465758473620
sezfabian <cheruiyotfabian@gmail.com>
2024-08-22 15:04:31 +0300

    postgres

--- dairydrive/settings.py
+++ dairydrive/settings.py
@@ -171,3 +171,6 @@ SIMPLE_JWT = {
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
