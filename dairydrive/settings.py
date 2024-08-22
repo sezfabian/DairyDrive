6bec7c6710d4077c1c194ebfc11b5c2bdd50f58a
sezfabian <cheruiyotfabian@gmail.com>
2024-08-23 00:10:27 +0300

    add allowed host

--- dairydrive/settings.py
+++ dairydrive/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-urahyrcw5-uk%hmo8q-#l=^k^8v+(3sqah^)5^$r=z+$ds2y!!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', '13.58.161.110']
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173", "http://172.16.0.103:5173",
 ]
