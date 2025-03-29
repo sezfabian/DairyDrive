e469a9d8b23dfbe1e6175012126f2cc08a3bd66a
Fabian Cheruiyot <cheruiyotfabian@gmail.com>
2025-03-30 02:03:46 +0300

    Update settings.py
    Added netlify host

--- dairydrive/settings.py
+++ dairydrive/settings.py
@@ -30,7 +30,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*', '13.58.161.110']
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173", "http://172.16.0.103:5173",
+    "http://localhost:5173", "http://172.16.0.103:5173", "https://dairydrive.netlify.app"
 ]
 
 # Application definition
