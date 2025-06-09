dc35ac9d7d775fd9f74b4cdb6832711336bb6f25
sezfabian <cheruiyotfabian@gmail.com>
2025-06-09 13:09:10 +0300

    updated transactions coce to charfield.

--- farms/models.py
+++ farms/models.py
@@ -71,7 +71,7 @@ class Transaction(models.Model):
     payment_method = models.CharField(max_length=20, choices=PAYMENT_METHODS)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     transaction_date = models.DateTimeField()
-    transaction_code = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    transaction_code = models.CharField(max_length=255, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
